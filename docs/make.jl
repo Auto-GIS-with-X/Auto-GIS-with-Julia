077477964103d2d7b3ea1b8992f7291c169e0b94
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2021-05-08 21:24:21 +0100

    update make

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ makedocs(;
     modules=[AutoGIS],
     authors="Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>",
     repo="https://github.com/Auto-GIS-with-X/Auto-GIS-with-Julia/blob/{commit}{path}#{line}",
-    sitename="AutoGIS.jl",
+    sitename="Auto GIS with Julia",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://auto-gis-with-x.github.io/Auto-GIS-with-Julia/",
@@ -19,5 +19,5 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="https://github.com/Auto-GIS-with-X/Auto-GIS-with-Julia",
+    repo="github.com/Auto-GIS-with-X/Auto-GIS-with-Julia",
 )
