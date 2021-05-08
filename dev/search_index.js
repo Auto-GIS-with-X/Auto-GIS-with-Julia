88fb81d5050730ab2f99839517bff1f2360f9fd7
Documenter.jl <documenter@juliadocs.github.io>
2021-05-08 20:30:14 +0000

    build based on e4f94c1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AutoGIS","category":"page"},{"location":"#AutoGIS","page":"Home","title":"AutoGIS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AutoGIS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AutoGIS]","category":"page"}]
+}
